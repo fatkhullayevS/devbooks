0ab29fb354e90af41bdebc605f27b97f6168f527
fatkhullayevS <zfathullayev@gmail.com>
2022-08-28 19:37:30 +0500

    dark mode light mode update

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,10 +1,16 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { createContext } from "react";
 
 export const ThemeContext = createContext()
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState("light")
+    const localData = window.localStorage.getItem("theme")
+    const [theme, setTheme] = useState(localData || "light")
+
+    useEffect(() => {
+        window.localStorage.setItem("theme", theme)
+    }, [theme])
 
     const data = {
         theme,
