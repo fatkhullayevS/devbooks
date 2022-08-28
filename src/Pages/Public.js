9f2adb15aeb024e9db7a15898750fe271fa885f1
fatkhullayevS <zfathullayev@gmail.com>
2022-08-28 18:52:11 +0500

    init

--- src/Pages/Public.js
+++ src/Pages/Public.js
@@ -5,7 +5,7 @@ import { Register } from "./Register/Register"
 export const Public = () => {
     return (
         <Routes>
-            <Route index path="/register" element={<Register />} />
+            <Route index path="/" element={<Register />} />
             <Route path="/login" element={<Login />} />
         </Routes>
     )
